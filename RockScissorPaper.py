8d8d66e63c062198fb8e7d93fa8f6460861b1edc
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-05-16 15:52:02 +0200

    Piedra Papel Tijera Act

--- RockScissorPaper.py
+++ RockScissorPaper.py
@@ -1,6 +1,7 @@
 #Piedra Papel Tijeras
-import random   #random.randtin, random.choice
-options = ["piedra", "papel", "tijera"]     #opciones 
+import string
+import random                               
+options = ["piedra", "papel", "tijera"]     
 play_again = "s"
 user_choice = ""
 
@@ -9,27 +10,30 @@ while play_again == "s":
     user_input = str(input("Elige ==> Piedra[r], Papel[p], Tijera[t]: ")).lower()
     
     #Piedra[r]  <=== por rock
-    #convierte tu input en piedra papel o tijeras
+    #asocia el input a la opción que corresponda [piedra, papel o tijera]
     if user_input == "r" or user_input == "piedra":
         user_choice = options[0]
     elif user_input == "p" or user_input == "papel":
         user_choice = options[1]
     elif user_input == "t" or user_input == "tijera":
         user_choice = options[2]
-    else: print("Wrong pick. Try again...")
+    else: 
+        user_input = 0
+        print("Opción inválida...")
       
-
-    #Compara tu elección contra la de la computadora
-    if user_choice == computer_choice:
-        print("Elegiste: {} vs Computadora: {} Empate!!".format(user_choice,computer_choice))
-    elif user_choice == options[0] and computer_choice == options[2]:
-        print("Elegiste: {} vs Computadora: {} Ganaste!!".format(user_choice, computer_choice))
-    elif user_choice == options[1] and computer_choice == options[0]:
-        print("Elegiste: {} vs Computadora: {} Ganaste!!".format(user_choice, computer_choice))
-    elif user_choice == options[2] and computer_choice == options[1]:
-        print("Elegiste: {} vs Computadora: {} Ganaste!!".format(user_choice, computer_choice))
-    else: print("Elegiste: {} vs Computadora: {} Perdiste!!".format(user_choice, computer_choice))
-
+    while user_input != 0:
+        #Compara tu elección contra la de la computadora
+        if user_choice == computer_choice:
+            print("Elegiste: {} vs Computadora: {} || Empate!!".format(user_choice,computer_choice))
+        elif user_choice == options[0] and computer_choice == options[2]:
+            print("Elegiste: {} vs Computadora: {} || Ganaste!!".format(user_choice, computer_choice))
+        elif user_choice == options[1] and computer_choice == options[0]:
+            print("Elegiste: {} vs Computadora: {} || Ganaste!!".format(user_choice, computer_choice))
+        elif user_choice == options[2] and computer_choice == options[1]:
+            print("Elegiste: {} vs Computadora: {} || Ganaste!!".format(user_choice, computer_choice))
+        else: print("Elegiste: {} vs Computadora: {} || Perdiste!!".format(user_choice, computer_choice))
+        break
+     
     #Pregunta si quieres jugar de nuevo
     play_again = input("Quieres jugar de nuevo: s/n ").lower()
     if play_again != "s":
