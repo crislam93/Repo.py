fc6dfde223cf64ef63cf5090f23dadcac91cff02
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-05-23 12:34:04 +0200

    create_csv.py *por terminar aun*

--- Google_CSV_Management/create_csv.py
+++ Google_CSV_Management/create_csv.py
@@ -2,28 +2,28 @@ import csv
 import os
 import datetime
 
+
 file_path  = open("Google_CSV_Management/data.txt")
 
 csv_reader = csv.reader(file_path)
 
-#for row in csv_reader:
- #   fullname, phone, role = row
-    #print("Name: {} || Phone: {} || Role: {}".format(fullname, phone, role))
+for row in csv_reader:
+    fullname, phone, role = row
+    print("Name: {} || Phone: {} || Role: {}".format(fullname, phone, role))
+
 
 def add_info(fullname, phone, role):
     fpath = "Google_CSV_Management/data.txt"
     with open(fpath, "a") as file:
-        for fullname in fpath:
-            if fullname in fpath:
-                print("Name {} Already Exists".format(fullname))
-                break
-            else:
-                file.writelines(fullname + ", ")
-                file.writelines(phone + ", ")
-                file.writelines(role + "\n")
-                timestamp      = os.path.getmtime(fpath)
-                modified_time  =  datetime.date.fromtimestamp(timestamp)
-                return "Last update {}, Data added: {}, {}, {}".format(modified_time,fullname,phone,role)+"\n"
+        if fullname in fpath:
+            print("Name {} Already Exists".format(fullname))
+        else:
+            file.writelines(fullname + ", ")
+            file.writelines(phone + ", ")
+            file.writelines(role + "\n")
+            timestamp      = os.path.getmtime(fpath)
+            modified_time  =  datetime.date.fromtimestamp(timestamp)
+            return "Last update {}, Data added: {}, {}, {}".format(modified_time,fullname,phone,role)
 
-print(add_info("Agregar Nombre", "Agregar Teléfono","Agregar Rol"))
+print(add_info("...", "123-654-789","..."))
 
